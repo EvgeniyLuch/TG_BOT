fab20698805d370a3c487da5b8b25c9f55ab4a69
EvgeniyLuch <150779618+EvgeniyLuch@users.noreply.github.com>
2025-11-24 08:12:14 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -119,7 +119,7 @@ async def daily_notifications():
         now = uz_now()
 
         # Нужное время — 09:00 по Узбекистану
-        target = now.replace(hour=8, minute=0, second=0, microsecond=0)
+        target = now.replace(hour=8, minute=30, second=0, microsecond=0)
 
         if now > target:
             target += datetime.timedelta(days=1)
@@ -157,4 +157,5 @@ async def daily_notifications():
                 await bot.send_message(user_id, text)
             except Exception as e:
                 print(f"Failed to send to {user_id}: {e}")
+
 
